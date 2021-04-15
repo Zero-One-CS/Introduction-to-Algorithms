cd9ecfb5c5921331e93fabf2c0fc2b621144b031
AhmdNassar <engahmdnassar@gmail.com>
2021-04-16 00:46:56 +0200

    Add Exercise 2 and Binary search imp

---
+++ ch2 - Getting Started/Exercises/Exercise 2/Exercise2.md
@@ -0,0 +1,12 @@
+# Exercise 2 
+
+Describe a ```nlog(n)``` time algorithm that, given a set S of n integers and another integer x, determines whether or not there exist two elements in S whose sum is exactly x.  
+### EX-1:
+Giving ```array = [2,3,4,8,20,13,17]``` and ``` x = 15 ```
+Answer should be: ```2,13``` as ```2 + 13 = 15```
+### EX-2:
+Giving ```array = [2,3,4,8,20,13,17]``` and ``` x = 30 ```
+Answer should be: ```Not Exist``` as we don't have any two numbers which there sum equal to 30
+### EX-3:
+Giving ```array = [0,2,3,4,8,20,13,17]``` and ``` x = 20 ```
+Answer should be: ```0,20``` or ```3,17``` as ```0 + 20 = 20 & 3 + 17 = 20 ```
---
+++ ch2 - Getting Started/Exercises/Solutions/1 - Binary search/BinarySearch.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+int binarySearch(int arr[], int size, int key)
+{
+   int midPoint, leftIdx = 0, rightIdx = size - 1;
+   while(leftIdx <= rightIdx)
+    {
+       midPoint = (leftIdx + rightIdx) / 2;
+       if(arr[midPoint] > key)
+       {
+           rightIdx = midPoint - 1;
+       }
+       else if(arr[midPoint] < key)
+       {
+           leftIdx = midPoint + 1;
+       }
+       else
+        return midPoint;
+   }
+   return -1;
+}
+
+int main()
+{
+    // 20 1 3 15 100 16 27 2
+    // 18
+
+
+
+
+
+    int testArr[] = {1,2,3,4,10,15,20,100};
+    int key = 1;
+    int sol = binarySearch(testArr, 8, key);
+    if(sol != -1)
+    {
+        cout<<"Found: "<<key<<" at index = "<<sol<<endl;
+    }
+    else
+        cout<<key<<" not exist in the Given Array\n";
+    return 0;
+}
