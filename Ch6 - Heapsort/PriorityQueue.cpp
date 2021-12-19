af1d09bed5b7b8eae8e3d540209108bdb3211bbb
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2021-12-19 15:54:25 +0200

    Priorty Queue Operations

--- Ch6 - Heapsort/PriorityQueue.cpp
+++ Ch6 - Heapsort/PriorityQueue.cpp
@@ -3,8 +3,42 @@
 
 using namespace std;
 
-int main () {
+void Inqueue(int arr[], int n, int x) //insert_with_priority
+{
+    arr[n] = x;
+    BuildMaxHeap(arr, n + 1);
+}
+
+int Dequeue(int arr[], int n) //remove the element from the queue that has the highest priority
+{
+    swap(arr[n], arr[0]);
+    int element = arr[n];
+    BuildMaxHeap(arr, n - 1);
+
+    return element;
+}
+
+int Peek(int arr[], int n)
+{
+    return arr[0];
+}
+
+int main()
+{
     int arr[] = {1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
-    PrintHeap(arr, sizeof(arr)/sizeof(arr[0]));
-    cout<< "Heap Sort" << endl;
+    int length = sizeof(arr) / sizeof(arr[0]);
+    cout << length;
+    BuildMaxHeap(arr, length);
+    cout << "Before Inserting" << endl;
+    PrintHeap(arr, length);
+    cout << "After Inserting" << endl;
+    Inqueue(arr, length, 20);
+    PrintHeap(arr, length + 1);
+    cout << "Dequeuing" << endl;
+    int returnedElement = Dequeue(arr, length);
+    PrintHeap(arr, length - 1);
+    cout << "Returned Element => Highest Priorty: " << returnedElement << endl;
+
+    int peekedElement = Peek(arr, length);
+    cout << "Highest Priorty: " << peekedElement << endl;
 }
